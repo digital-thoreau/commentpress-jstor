1a4058e50c67afacae611e77808332ee85cf185d
Christian Wach <needle@haystack.co.uk>
2016-02-10 14:51:29 +0000

    bump version to 0.1.1

--- commentpress-jstor.php
+++ commentpress-jstor.php
@@ -4,7 +4,7 @@ Plugin Name: CommentPress JSTOR
 Plugin URI: http://www.futureofthebook.org/commentpress/
 Description: Allows CommentPress to access JSTOR Matchmaker tool from The Reader's Thoreau site.
 Author: Institute for the Future of the Book
-Version: 0.1
+Version: 0.1.1
 Author URI: http://www.futureofthebook.org
 Text Domain: commentpress-jstor
 Domain Path: /languages
@@ -14,7 +14,7 @@ Domain Path: /languages
 
 
 // set our version here
-define( 'COMMENTPRESS_JSTOR_VERSION', '0.1' );
+define( 'COMMENTPRESS_JSTOR_VERSION', '0.1.1' );
 
 // store reference to this file
 if ( ! defined( 'COMMENTPRESS_JSTOR_FILE' ) ) {
